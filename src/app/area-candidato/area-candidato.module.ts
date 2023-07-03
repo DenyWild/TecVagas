cd1b1059e5951052009b05978ed2c5bb74d8d946
DenyWild <denyswildson2011@gmail.com>
2023-07-03 19:49:06 -0300

    Sending changes with modules who are without import app-material module

--- src/app/area-candidato/area-candidato.module.ts
+++ src/app/area-candidato/area-candidato.module.ts
@@ -1,17 +1,9 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { AppMaterialModule } from './../shared/app-material/app-material.module';
 import { AreaCandidatoRoutingModule } from './area-candidato-routing.module';
 import { AreaCandidatoComponent } from './area-candidato/area-candidato.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { FormsModule } from '@angular/forms';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
@@ -21,15 +13,7 @@ import { MatInputModule } from '@angular/material/input';
   imports: [
     CommonModule,
     AreaCandidatoRoutingModule,
-    MatButtonModule,
-    MatIconModule,
-    MatSlideToggleModule,
-    FormsModule,
-    MatToolbarModule,
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatCardModule,
-    MatInputModule
+    AppMaterialModule
   ]
 })
 export class AreaCandidatoModule { }
--- src/app/cadastro-candidato/cadastro-candidato.module.ts
+++ src/app/cadastro-candidato/cadastro-candidato.module.ts
@@ -1,17 +1,9 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { CadastroCandidatoRoutingModule } from './cadastro-candidato-routing.module';
 import { CadastroCandidatoComponent } from './cadastro-candidato/cadastro-candidato.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { FormsModule } from '@angular/forms';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -20,15 +12,7 @@ import { MatInputModule } from '@angular/material/input';
   imports: [
     CommonModule,
     CadastroCandidatoRoutingModule,
-    MatButtonModule,
-    MatIconModule,
-    MatSlideToggleModule,
-    FormsModule,
-    MatToolbarModule,
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatCardModule,
-    MatInputModule
+    AppMaterialModule
   ]
 })
 export class CadastroCandidatoModule { }

fbe71d9a59f547081dcc849e3696bf937af95751
DenyWild <denyswildson2011@gmail.com>
2023-07-04 18:26:47 -0300

    Visual pages about candidate and companies informations made

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { SCandidatoModule } from './s-candidato/s-candidato.module';
+import { SCandidatoComponent } from './s-candidato/s-candidato/s-candidato.component';
 import { CursosCandidatoComponent } from './cursos-candidato/cursos-candidato/cursos-candidato.component';
 import { HomeComponent } from './home/home/home.component';
 import { NgModule } from '@angular/core';
@@ -5,6 +7,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeModule } from './home/home.module';
 import { CursosCandidatoModule } from './cursos-candidato/cursos-candidato.module';
+import { SEmpresaComponent } from './s-empresa/s-empresa/s-empresa.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home'},
@@ -12,6 +15,14 @@ const routes: Routes = [
   { path: 'cursos', component: CursosCandidatoComponent},
   { path: 'cursos',
   loadChildren: () => import('./cursos-candidato/cursos-candidato.module').then(m => m.CursosCandidatoModule)
+  },
+  { path: 'scandidato', component: SCandidatoComponent},
+  { path: 'scandidato',
+  loadChildren: () => import('./s-candidato/s-candidato.module').then(m => m.SCandidatoModule)
+  },
+  { path: 'sempresa', component: SEmpresaComponent},
+  { path: 'sempresa',
+  loadChildren: () => import('./s-empresa/s-empresa.module').then(m => m.SEmpresaModule)
   }
 
 ];
